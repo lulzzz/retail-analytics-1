dcf7e921603001982fe8ef8e00cd3078ad0d079b
Rajnish  Kumar <rajnishkumar.s@flipkart.com>
2017-04-06 18:38:18 +0530

    RETAILVM-99 changes

--- manager/src/main/java/com/flipkart/retail/analytics/config/AnalyticsConfiguration.java
+++ manager/src/main/java/com/flipkart/retail/analytics/config/AnalyticsConfiguration.java
@@ -1,5 +1,8 @@
 package com.flipkart.retail.analytics.config;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 import fk.sp.common.extensions.GraphiteConfig;
 import fk.sp.common.extensions.dropwizard.db.HasDataSourceFactory;
 import fk.sp.common.extensions.swagger.HasSwaggerConfiguration;
@@ -11,32 +14,30 @@ import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.db.DataSourceFactory;
 import lombok.Data;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
 @Data
 public class AnalyticsConfiguration extends Configuration implements HasDataSourceFactory,
-        HasSwaggerConfiguration, HasGraphiteConfig {
-    @Valid
-    private DataSourceFactory dataSource;
+								     HasSwaggerConfiguration,
+								     HasGraphiteConfig {
+  @Valid
+  private DataSourceFactory dataSource;
 
-    @Valid
-    private GraphiteConfig graphiteConfig = null;
+  @Valid
+  private GraphiteConfig graphiteConfig = null;
 
-    @Valid
-    @NotNull
-    private RotationManagementConfig rotationManagementConfig;
-    @Valid
-    @NotNull
-    private SwaggerConfiguration swaggerConfiguration = new SwaggerConfiguration();
+  @Valid
+  @NotNull
+  private RotationManagementConfig rotationManagementConfig;
+  @Valid
+  @NotNull
+  private SwaggerConfiguration swaggerConfiguration = new SwaggerConfiguration();
 
-    @Override
-    public DataSourceFactory getDatabaseConfiguration() {
-        return dataSource;
-    }
+  @Override
+  public DataSourceFactory getDatabaseConfiguration() {
+    return dataSource;
+  }
 
-    @NotNull
-    @Valid
-    JerseyClientConfiguration jerseyClient;
+  @NotNull
+  @Valid
+  JerseyClientConfiguration jerseyClient;
 
 }
--- manager/src/main/java/com/flipkart/retail/analytics/config/AnalyticsModule.java
+++ manager/src/main/java/com/flipkart/retail/analytics/config/AnalyticsModule.java
@@ -1,33 +1,36 @@
 package com.flipkart.retail.analytics.config;
 
+import com.google.common.collect.Lists;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.multibindings.Multibinder;
+
 import com.flipkart.retail.analytics.persistence.AggregatedPaymentsManager;
 import com.flipkart.retail.analytics.persistence.PaymentsManager;
 import com.flipkart.retail.analytics.persistence.impl.AggregatedPaymentsManagerImpl;
 import com.flipkart.retail.analytics.persistence.impl.PaymentsManagerImpl;
 import com.flipkart.retail.analytics.resources.AggregatedPaymentResource;
 import com.flipkart.retail.analytics.resources.PaymentResource;
-import com.google.inject.AbstractModule;
-import com.google.inject.Provider;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.google.inject.multibindings.Multibinder;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import fk.sp.common.extensions.GraphiteConfig;
 import fk.sp.common.extensions.dropwizard.db.HasDataSourceFactory;
 import fk.sp.sa.reports.update.ReportDefinitionFile;
 import flipkart.retail.server.admin.config.RotationManagementConfig;
 import io.dropwizard.client.JerseyClientConfiguration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
-import java.time.Clock;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AnalyticsModule extends AbstractModule {
 
-  List<String> darwinFiles =  new ArrayList();
-
-
+  List<String> darwinFiles = new ArrayList();
 
   @Provides
   @Singleton
@@ -35,7 +38,6 @@ public class AnalyticsModule extends AbstractModule {
       Provider<AnalyticsConfiguration> provider) {
     return provider.get().getJerseyClient();
   }
-
 
   @Provides
   @Singleton
@@ -51,40 +53,36 @@ public class AnalyticsModule extends AbstractModule {
 
   @Provides
   public HasDataSourceFactory providesHasDataSourceFactory(
-          AnalyticsConfiguration analyticsConfiguration) {
+      AnalyticsConfiguration analyticsConfiguration) {
     return () -> analyticsConfiguration.getDatabaseConfiguration();
   }
 
-
   @Provides
   GraphiteConfig providesGraphiteConfig(
-          final Provider<AnalyticsConfiguration> sellerAnalyticsConfigurationProvider) {
+      final Provider<AnalyticsConfiguration> sellerAnalyticsConfigurationProvider) {
     return sellerAnalyticsConfigurationProvider.get().getGraphiteConfig();
   }
 
   @Provides
   JdbcTemplate providesJdbcTemplate(DataSource dataSource)
-          throws ClassNotFoundException {
+  throws ClassNotFoundException {
     return new JdbcTemplate(dataSource);
   }
-
 
   @Override
   protected void configure() {
 
-    List<String> darwinFiles = new ArrayList<String>();
-    darwinFiles.add("reports/config.yaml");
+    List<String> darwinFiles =
+	Lists.newArrayList("purchase_order_aggr.yaml", "return_order_aggr.yaml", "irn_aggr.yaml");
 
     Multibinder<ReportDefinitionFile> multiBinder = Multibinder.newSetBinder(
-            binder(),
-            ReportDefinitionFile.class
+	binder(),
+	ReportDefinitionFile.class
     );
     darwinFiles.stream().forEach(
-            reportDefinitionFile -> multiBinder
-                    .addBinding()
-                    .toInstance(new ReportDefinitionFile(reportDefinitionFile)));
-
-//    bind(HasDataSourceFactory.class).to(AnalyticsConfiguration.class);
+	reportDefinitionFile -> multiBinder
+	    .addBinding()
+	    .toInstance(new ReportDefinitionFile(reportDefinitionFile)));
 
     //resource binding
     bind(PaymentResource.class).in(Singleton.class);
@@ -92,9 +90,8 @@ public class AnalyticsModule extends AbstractModule {
 
     //interface and implementation binding
     bind(PaymentsManager.class).to(PaymentsManagerImpl.class).in(Singleton.class);
-    bind(AggregatedPaymentsManager.class).to(AggregatedPaymentsManagerImpl.class).in(Singleton.class);
-
-
+    bind(AggregatedPaymentsManager.class).to(AggregatedPaymentsManagerImpl.class)
+	.in(Singleton.class);
 
   }
 }
