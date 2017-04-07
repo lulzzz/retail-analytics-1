7c9cd65d189a87ec0492e0dd007e92985152301e
vibhuti-agg <vibhuti.a@PW-C02NN4MDG3QJ.local>
2017-04-07 14:35:01 +0530

    Invoice intergration with ADS

--- manager/src/main/java/com/flipkart/retail/analytics/config/AnalyticsModule.java
+++ manager/src/main/java/com/flipkart/retail/analytics/config/AnalyticsModule.java
@@ -1,32 +1,28 @@
 package com.flipkart.retail.analytics.config;
 
-import com.google.common.collect.Lists;
-import com.google.inject.AbstractModule;
-import com.google.inject.Provider;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.google.inject.multibindings.Multibinder;
-
 import com.flipkart.retail.analytics.persistence.AggregatedPaymentsManager;
 import com.flipkart.retail.analytics.persistence.PaymentsManager;
 import com.flipkart.retail.analytics.persistence.impl.AggregatedPaymentsManagerImpl;
 import com.flipkart.retail.analytics.persistence.impl.PaymentsManagerImpl;
 import com.flipkart.retail.analytics.resources.AggregatedPaymentResource;
 import com.flipkart.retail.analytics.resources.PaymentResource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.time.Clock;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.sql.DataSource;
-
+import com.google.common.collect.Lists;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.multibindings.Multibinder;
 import fk.sp.common.extensions.GraphiteConfig;
 import fk.sp.common.extensions.dropwizard.db.HasDataSourceFactory;
 import fk.sp.sa.reports.update.ReportDefinitionFile;
 import flipkart.retail.server.admin.config.RotationManagementConfig;
 import io.dropwizard.client.JerseyClientConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnalyticsModule extends AbstractModule {
 
@@ -73,7 +69,8 @@ public class AnalyticsModule extends AbstractModule {
   protected void configure() {
 
     List<String> darwinFiles =
-	Lists.newArrayList("reports/purchase_order_aggr.yaml", "reports/return_order_aggr.yaml", "reports/irn_aggr.yaml");
+	Lists.newArrayList("reports/purchase_order_aggr.yaml", "reports/return_order_aggr.yaml", "reports/irn_aggr.yaml",
+            "reports/invoice_aggr.yaml");
 
     Multibinder<ReportDefinitionFile> multiBinder = Multibinder.newSetBinder(
 	binder(),
